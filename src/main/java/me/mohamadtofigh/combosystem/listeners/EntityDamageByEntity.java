e6ee08c1836dc6d71157b24a3b70e96f53591038
mohamadtofigh <mohamadtofigh@outlook.com>
2024-01-05 16:06:19 +0330

    creating plugin

--- src/main/java/me/mohamadtofigh/combosystem/listeners/EntityDamageByEntity.java
+++ src/main/java/me/mohamadtofigh/combosystem/listeners/EntityDamageByEntity.java
@@ -1,18 +1,18 @@
 package me.mohamadtofigh.combosystem.listeners;
 
-import org.bukkit.entity.Entity;
+import me.mohamadtofigh.combosystem.utils.config.Config;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
-public class EntityDamageByEntity implements Listener {
+public class EntityDamageByEntity implements Listener, Config {
 
     @EventHandler
     public static void onDamage(EntityDamageByEntityEvent event){
         if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
             Player player = (Player) event.getEntity();
-            player.setVelocity(player.getLocation().getDirection().multiply(-2));
+            player.setVelocity(player.getLocation().getDirection().multiply(getLaunching));
         }
     }
 }
--- src/main/java/me/mohamadtofigh/combosystem/utils/config/Config.java
+++ src/main/java/me/mohamadtofigh/combosystem/utils/config/Config.java
@@ -1,4 +1,27 @@
 package me.mohamadtofigh.combosystem.utils.config;
 
+import me.mohamadtofigh.combosystem.Access;
+import org.bukkit.configuration.Configuration;
+
 public interface Config {
+    Configuration config = Access.getMain.getConfig();
+    String getConfigVersion = config.getString("Version");
+    String getUserToken = config.getString("Root.Token");
+    boolean getExe = config.getBoolean("Exe");
+    int getLaunching = config.getInt("Combo.Launching");
+    String getPermAdmin = config.getString("Root.Permissions.Admin");
+    String getPermReload = config.getString("Root.Permissions.Reload");
+    String getPermDebug = config.getString("Root.Permissions.Debug");
+    String getPrefix = config.getString("Messages.Prefix");
+    String reloadMessage = config.getString("Messages.Reload");
+    String debugMessage = config.getString("Messages.Debug");
+    default String getReloadMessage(){
+        return reloadMessage.replace("{prefix}", getPrefix);
+    }
+    default String getDebugMessage(String playerName, String debugMessages){
+        return debugMessage
+                .replace("{prefix}", getPrefix)
+                .replace("{player}", playerName)
+                .replace("{dev_message}", debugMessages);
+    }
 }
